d8f98574a1a56ed81cb146bd4e71d168b785d3b7
Daniel Hollands <daniel@limeblast.co.uk>
2017-03-07 14:57:31 +0000

    Based on feedback from pimoroni, updated the echo method

--- echo.py
+++ echo.py
@@ -11,12 +11,8 @@ if len(sys.argv) != 2:
     print("\nusage: python echo.py \"message\" \npress CTRL-C to exit\n")
     sys.exit(0)
 
-string = sys.argv[1]
-length = len(string)
-width = 17
-buffer = (length * 6) + width
-
-scrollphathd.write_string(string + "      ", x=width, y=0, font=font5x7, brightness=0.5)
+string = sys.argv[1] + '      '
+buffer = scrollphathd.write_string(string, x=17, y=0, font=font5x7, brightness=0.5)
 
 for i in range(buffer):
     scrollphathd.show()
