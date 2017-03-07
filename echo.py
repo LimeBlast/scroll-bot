9985480b141bf837dc1242c9339e3c2775036647
Daniel Hollands <daniel@limeblast.co.uk>
2017-03-07 15:52:56 +0000

    I appear to have had a breakthrough, so I'm comitting before I break it

--- echo.py
+++ echo.py
@@ -18,3 +18,29 @@ for i in range(buffer):
     scrollphathd.show()
     scrollphathd.scroll(1)
     time.sleep(0.05)
+
+scrollphathd.scroll_to(0, 0)
+scrollphathd.show()
+scrollphathd.clear()
+scrollphathd.show()
+
+string = 'something else      '
+buffer = scrollphathd.write_string(string, x=17, y=0, font=font5x7, brightness=0.5)
+
+for i in range(buffer):
+    scrollphathd.show()
+    scrollphathd.scroll(1)
+    time.sleep(0.05)
+
+scrollphathd.scroll_to(0, 0)
+scrollphathd.show()
+scrollphathd.clear()
+scrollphathd.show()
+
+string = 'and another one      '
+buffer = scrollphathd.write_string(string, x=17, y=0, font=font5x7, brightness=0.5)
+
+for i in range(buffer):
+    scrollphathd.show()
+    scrollphathd.scroll(1)
+    time.sleep(0.05)
